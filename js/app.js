0b24e01129e74493f00f2fe939f71b98b4b1fdb3
Erin Johnson <erinjohnson47@gmail.com>
2019-07-25 12:46:29 -0700

    another fact

--- js/app.js
+++ js/app.js
@@ -7,9 +7,9 @@ const randomRGB = () => {
 }
 randomRGB();
 
-const facts = ["Leah is Funny AF!", "Leah is SUPER SMART", "Leah's plants are growing like gangbusters", "Leah has cute cats with cool names", "Leah laughs at my jokes", "Leah makes me laugh", "Leah helps me when I get stuck on a coding problem", "Leah is fun to hang out with", "Leah is an honorable carpenter-ant warrior", "Leah and Erin never run out of things to laugh and talk about!"]
+const facts = ["Leah is Funny AF!", "Leah is SUPER SMART", "Leah's plants are growing like gangbusters!!!", "Leah has cute cats with cool names.", "Leah laughs at my jokes", "Leah makes me laugh", "Leah helps me when I get stuck on a coding problem.", "Leah is fun to hang out with", "Leah is an honorable carpenter-ant warrior.", "Leah and Erin never run out of things to laugh and talk about!", "Leah isn't fooled by the rocks that I got."]
 
-const random = [45, 376, 11, 237, 68, 92, 183, 8, 109, 88]
+const random = [45, 376, 11, 237, 68, 92, 183, 8, 109, 88, 16]
 
 const $button = $('button');
 const $leah = $('#Leah');
