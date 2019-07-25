3ff37f0cf3f8ee1e66cc9e8e4e1904875c567103
Erin Johnson <erinjohnson47@gmail.com>
2019-07-25 12:41:05 -0700

    hbd Leah

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const randomRGB = () => {
+    let red, green, blue;
+    red = Math.floor(Math.random() *256);
+    green = Math.floor(Math.random() *256);
+    blue = Math.floor(Math.random() *256);
+    $('body').css("background-color", `rgb(${red}, ${green}, ${blue}`);
+}
+randomRGB();
+
+const facts = ["Leah is Funny AF!", "Leah is SUPER SMART", "Leah's plants are growing like gangbusters", "Leah has cute cats with cool names", "Leah laughs at my jokes", "Leah makes me laugh", "Leah helps me when I get stuck on a coding problem", "Leah is fun to hang out with", "Leah is an honorable carpenter-ant warrior", "Leah and Erin never run out of things to laugh and talk about!"]
+
+const random = [45, 376, 11, 237, 68, 92, 183, 8, 109, 88]
+
+const $button = $('button');
+const $leah = $('#Leah');
+
+$button.on('click', () => {
+    let answerNum = Math.floor(Math.random()* Math.floor(facts.length));
+    $leah.text((`Why I love Leah reason number ${random[answerNum]}: ${facts[answerNum]}.`))
+    randomRGB();
+}) 
